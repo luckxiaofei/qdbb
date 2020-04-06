a9303a389a611884131f908c4de30ed7f5327c5d
luck <luckxiaofei@outlook.com>
2020-04-07 01:13:21 +0800

    1.0

--- renard-wx/config/api.js
+++ renard-wx/config/api.js
@@ -1,11 +1,11 @@
 // 以下是业务服务器API地址
 // 本机开发时使用
-var WxApiRoot = 'https://localhost:8080/wx/';
+// var WxApiRoot = 'https://localhost:8080/wx/';
 // 局域网测试使用
 // var WxApiRoot = 'http://47.115.124.84:8080/wx/';
 // 云平台上线时使用
 // var WxApiRoot = 'https://litemall.menethil.com.cn/wx/';
-// var WxApiRoot = 'https://www.qdbb.ltd:8080/wx/';
+var WxApiRoot = 'https://www.qdbb.ltd:8080/wx/';
 
 module.exports = {
     IndexUrl: WxApiRoot + 'home/index', //首页数据接口
